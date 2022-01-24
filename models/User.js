7a2c2019bab976ec08174b1f271c321fbd9eb486
kimjongseok <kimjongseok@bouncecode.io>
2022-01-24 09:38:36 +0900

    토큰 생성

--- models/User.js
+++ models/User.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose
 
 //bcrypt 라이브러리 사용
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 
 //암호할 키값이 몇글자 인지 설정
 const saltRounds = 10;
+
+//jwt
+const jwt = require('jsonwebtoken');
+
 
 const userSchema = new Schema({
     name: {
@@ -66,8 +70,30 @@ userSchema.pre('save', function(next){
             next()
         })
     })
+    } else {
+        next()
     }
 })
+
+// 아래의 'comparePassword' 변경가능 / 입력한 비밀번호를 비교하는 메소드
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    // plainPassword = '1234' 와 암호화된 비밀번호가 같은지 체크
+    bcrypt.compare(plainPassword, this.password, function(err,isMatch){
+        if(err) return cb(err);
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    //jwt 토큰 생성
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(),'secretToken')
+    user.token = token
+    user.save(function(err,user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
 
 // 다른 곳에서 사용 가능하도록 모델을 expoert 해줌
 module.exports = mongoose.model('User', userSchema)
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const server = express();
 const User = require('./models/User')
 const bodyParser = require('body-parser')
-
+const cookieParser = require('cookie-parser')
 //.env 파일에서 직접 path값을 받아오기 {아래의 코드를 사용한다면 process.env.MONGODB_URL 로 사용, variables.env파일 참조}
 // require('dotenv').config({ path: 'variables.env' })
 
@@ -17,7 +17,8 @@ server.use(bodyParser.urlencoded({extended: true}))
 
 // json 타입 분석
 server.use(bodyParser.json())
-
+// cookie 타입 분석
+server.use(cookieParser())
 
 server.get('/',(req,res)=>{
     res.send("<h1>Hello from<h1>\n<h2>ㅎㅎ<h2>")
@@ -52,7 +53,34 @@ server.post('/register', (req,res) => {
     })
 })
 
+server.post('/login', (req,res) => {
+    // 요청된 이메일이 데이터 베이스에 있는지 확인 (findOne은 몽고DB함수)
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if(!user) {
+            return res.json({ 
+                loginSuccess: false,
+                message: '가입되지 않은 이메일 주소 입니다.'
+            })
+        }
 
+    // 요청된 이메일이 데이터 베이스에 있다면, 비밀번호가 동일한지 확인
+    user.comparePassword( req.body.password, (err, isMatch) => {
+        if(!isMatch)
+            return res.json({ loginSuccess: false, message: '비밀번호가 틀렸습니다.'});
+        
+            // 비밀번호까지 맞다면, 토큰을 생성하기
+
+        user.generateToken((err, user) => {
+            if(err) return res.status(400).send(err);
+
+            // 토큰을 저장한다. (쿠키, 로컬스토리지, 세션 등...) 일단 쿠키에 저장 , 쿠키파서 설치
+            res.cookie('x_auth',user.token)
+            .status(200)
+            .json({ loginSuccess: true, userId: user._id })
+            })
+        })
+    })
+})
 
 server.listen(3000,(err)=>{
     if(err) {
